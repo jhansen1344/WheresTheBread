6879f0854c97f1df6dbdebade0913bdc8afbdb5f
Jeremy Hansen <jhansen1344@gmail.com>
2020-06-27 16:48:38 -0400

    item dto added

--- WheresTheBread.DTO/ItemDto/ItemUpdateDto.cs
+++ WheresTheBread.DTO/ItemDto/ItemUpdateDto.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace WheresTheBread.DTO.ItemDto
 {
     public class ItemUpdateDto
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Too many characters")]
+        [Display(Name = "Item Name")]
+        public string Name { get; set; }
+        public int? LocationId { get; set; }
+  
     }
 }
